01a884462993fdbc722fe44cc75fe48efb0a642b
Christian Alberto Vázquez Cruz <zS22013636@estudiantes.uv.mx>
2025-03-04 21:50:37 -0600

    Se añadio getUserPhotos

--- src/controllers/fotoController.js
+++ src/controllers/fotoController.js
@@ -14,3 +14,15 @@ exports.uploadPhoto = async (req, res) => {
     res.status(500).json({ error: "Error al subir la foto" });
   }
 };
+
+// Obtener todas las fotos de un usuario
+exports.getUserPhotos = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const photos = await Photo.find({ userId });
+
+    res.status(200).json(photos);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener las fotos" });
+  }
+};
